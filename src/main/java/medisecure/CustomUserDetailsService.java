efa19e2f94e17a260ce6ed0472a12761882961d3
Shoaib Hussain <shoaibhussain@Shoaibs-MacBook-Air.local>
2026-05-26 09:49:10 -0400

    Fixed login email case sensitivity

--- src/main/java/medisecure/CustomUserDetailsService.java
+++ src/main/java/medisecure/CustomUserDetailsService.java
@@ -20,6 +20,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
+        email = email.trim().toLowerCase();
+
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
